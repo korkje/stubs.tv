9fcf7bc019cca9f84987e052344ca5e7431ef5db
Ola Næss Kaldestad <mail@okal.no>
2026-08-10 18:51:48 +0200

    horizontal logo/type

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,11 +5,15 @@ import { StubsMark } from "@/components/StubsMark";
 export default function Home() {
   return (
     <Container size="2" px="4">
-      <Flex direction="column" align="center" gap="4" py="9">
-        <Box style={{ transform: "rotate(6deg)" }} mb="2">
-          <StubsMark width={120} />
-        </Box>
-        <Heading size="9">stubs</Heading>
+      <Flex direction="column" align="center" gap="8" py="9">
+        <Flex direction="row" align="center" gap="4">
+          <Box style={{ transform: "rotate(-6deg)" }}>
+            <StubsMark width={120} />
+          </Box>
+          <Heading size="9">
+            stubs.tv
+          </Heading>
+        </Flex>
         <Text size="4" color="gray" align="center">
           Keep your ticket stubs. Track the movies and TV shows you watch, and
           see your watching life laid out in front of you.
